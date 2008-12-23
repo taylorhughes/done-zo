b27e621aad5327591575125d7d18e67b0e4ede2d
tdawg <tdawg@c02d1e6f-6a35-45f2-ab14-3b6f79a691ff>
2008-12-23 04:00:35 +0000

    Strip down Django setting by getting rid of things we don't use, which is almost everything.
    git-svn-id: svn+ssh://taylor-hughes.com/svnroot/dnzo/trunk@156 c02d1e6f-6a35-45f2-ab14-3b6f79a691ff

--- dnzo/settings.py
+++ dnzo/settings.py
@@ -1,68 +1,32 @@
-# Django settings for taylor project.
-import os
 import environment
 
 DEBUG          = environment.IS_DEVELOPMENT
 TEMPLATE_DEBUG = environment.IS_DEVELOPMENT
 
-ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
-    ('Taylor Hughes', 'taylor@taylor-hughes.com'),
-)
-
-MANAGERS = ADMINS
-
-# Local time zone for this installation. Choices can be found here:
-# http://www.postgresql.org/docs/8.1/static/datetime-keywords.html#DATETIME-TIMEZONE-SET-TABLE
-# although not all variations may be possible on all operating systems.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'America/Chicago'
-
-# Language code for this installation. All choices can be found here:
-# http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
-# http://blogs.law.harvard.edu/tech/stories/storyReader$15
-LANGUAGE_CODE = 'en-us'
-
-SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
 USE_I18N = False
 
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = '1u#s89y*!8809d&ogmw^c@*9sz+$8_rlxfmk-s6&*uv9&t2fz%'
-
-# List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
-#     'django.template.loaders.eggs.load_template_source',
+  # Load basic template files in the normal way
+  'django.template.loaders.filesystem.load_template_source',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.core.context_processors.debug',
-#    'django.core.context_processors.i18n',
 ) 
 
 MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.doc.XViewMiddleware',
+  # does things like APPEND_SLASH for URLs
+  'django.middleware.common.CommonMiddleware',
 )
 
 ROOT_URLCONF = 'urls'
 
+import os
 ROOT_PATH = os.path.dirname(__file__)
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
     ROOT_PATH + '/resources/templates'
 )
 
 INSTALLED_APPS = (
-    'django.contrib.contenttypes',
-    'django.contrib.sites',
     'tasks',
     'public',
     'admin',
