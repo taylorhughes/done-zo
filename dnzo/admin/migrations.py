0c6240a7b31cc1dddaa7d1e61f00275c250197ab
tdawg <tdawg@c02d1e6f-6a35-45f2-ab14-3b6f79a691ff>
2009-01-15 06:13:52 +0000

    Add migration to update Project entities as well.
    git-svn-id: svn+ssh://taylor-hughes.com/svnroot/dnzo/trunk@186 c02d1e6f-6a35-45f2-ab14-3b6f79a691ff

--- dnzo/admin/migrations.py
+++ dnzo/admin/migrations.py
@@ -32,11 +32,48 @@ def update_project_slugs(start_key = None):
     total += 1
   
   return (total, updated, last_key)
+  
+def update_project_entities(start_key = None):
+  max_records = 50
+  
+  params = {}
+  wheres = []
+  
+  if start_key is not None:
+    wheres.append('__key__ > :key')
+    params['key'] = db.Key(start_key)
+  
+  if len(wheres) > 0:
+    wheres = 'WHERE ' + ' AND '.join(wheres)
+  else:
+    wheres = ''
+  
+  from tasks_data.models import Project
+  from util.misc import slugify
+  
+  projects = Project.gql('%s ORDER BY __key__ ASC' % wheres, **params).fetch(max_records)
+  
+  total = 0
+  updated = 0
+  last_key = None
+  for project in projects:
+    project.short_name = slugify(project.name)
+    project.put()
+    updated += 1
+    total += 1
+    last_key = str(project.key())
+  
+  return (total, updated, last_key)
 
 MIGRATIONS = [
   {
     'name': 'Update project slugs', 
     'slug': 'update_project_slugs', 
     'migration': update_project_slugs
+  },
+  {
+    'name': 'Update project entities', 
+    'slug': 'update_project_entities', 
+    'migration': update_project_entities
   }
 ]
