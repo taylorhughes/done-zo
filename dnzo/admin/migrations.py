35029daf882626cbdda7d506e3f2bc60db9bd5a0
tdawg <tdawg@c02d1e6f-6a35-45f2-ab14-3b6f79a691ff>
2009-01-25 19:47:42 +0000

    Add a migration to set real_created_at.
    git-svn-id: svn+ssh://taylor-hughes.com/svnroot/dnzo/trunk@203 c02d1e6f-6a35-45f2-ab14-3b6f79a691ff

--- dnzo/admin/migrations.py
+++ dnzo/admin/migrations.py
@@ -1,6 +1,6 @@
 from google.appengine.ext import db
 
-def update_project_slugs(start_key = None):
+def update_created_at(start_key = None):
   max_records = 100
   
   params = {}
@@ -16,7 +16,6 @@ def update_project_slugs(start_key = None):
     wheres = ''
   
   from tasks_data.models import Task
-  from util.misc import slugify
   
   tasks = Task.gql('%s ORDER BY __key__ ASC' % wheres, **params).fetch(max_records)
   
@@ -24,56 +23,20 @@ def update_project_slugs(start_key = None):
   updated = 0
   last_key = None
   for task in tasks:
-    if task.project:
-      task.project_index = slugify(task.project)
-      task.put()
-      updated += 1
-    last_key = str(task.key())
-    total += 1
-  
-  return (total, updated, last_key)
-  
-def update_project_entities(start_key = None):
-  max_records = 50
-  
-  params = {}
-  wheres = []
-  
-  if start_key is not None:
-    wheres.append('__key__ > :key')
-    params['key'] = db.Key(start_key)
-  
-  if len(wheres) > 0:
-    wheres = 'WHERE ' + ' AND '.join(wheres)
-  else:
-    wheres = ''
-  
-  from tasks_data.models import Project
-  from util.misc import slugify
-  
-  projects = Project.gql('%s ORDER BY __key__ ASC' % wheres, **params).fetch(max_records)
-  
-  total = 0
-  updated = 0
-  last_key = None
-  for project in projects:
-    project.short_name = slugify(project.name)
-    project.put()
+    task.real_created_at = task.created_at
+    task.put()
+
     updated += 1
     total += 1
-    last_key = str(project.key())
+
+    last_key = str(task.key())
   
   return (total, updated, last_key)
-
+  
 MIGRATIONS = [
   {
-    'name': 'Update project slugs', 
-    'slug': 'update_project_slugs', 
-    'migration': update_project_slugs
-  },
-  {
-    'name': 'Update project entities', 
-    'slug': 'update_project_entities', 
-    'migration': update_project_entities
+    'name': 'Update real crated_at times', 
+    'slug': 'update_created_at', 
+    'migration': update_created_at
   }
 ]
