a27dbae001bcef152cc79a5be3dde9e4c5e2eb64
tdawg <tdawg@c02d1e6f-6a35-45f2-ab14-3b6f79a691ff>
2009-02-02 02:01:09 +0000

    Added data migration for projects/contexts mru lists.
    git-svn-id: svn+ssh://taylor-hughes.com/svnroot/dnzo/trunk@228 c02d1e6f-6a35-45f2-ab14-3b6f79a691ff

--- dnzo/admin/migrations.py
+++ dnzo/admin/migrations.py
@@ -1,8 +1,47 @@
 from google.appengine.ext import db
 
-def update_created_at(start_key = None):
-  max_records = 100
+def update_mru_lists(start_key = None):
+  def fn(user):
+    from tasks_data.models import ProjectIndex
+    from tasks_data.models import Context
+    
+    contexts = Context.gql('WHERE ANCESTOR IS :user', user=user).fetch(200)
+    contexts.sort(key=lambda c: c.last_used_at)
+    contexts.reverse()
+    user.mru_contexts = map(lambda c: c.name, contexts)
+    
+    projects = ProjectIndex.gql('WHERE ANCESTOR IS :user', user=user).fetch(200)
+    projects.sort(key=lambda p: p.last_used_at)
+    projects.reverse()
+    projects = map(lambda p: p.name, projects)
+    projects_new = []
+    for project in projects:
+      if project not in projects_new:
+        projects_new.append(project)
+    user.mru_projects = projects_new
+    
+    from tasks_data.users import save_user
+    save_user(user)
+    
+  from tasks_data.models import TasksUser
+  return do_for_all(TasksUser, start_key, fn, 1)
+
+def do_for_all(model_klass, start_key, callback, max_records = 100):
+  objects = get_all_from_key(model_klass, start_key, max_records)
   
+  total = 0
+  updated = 0
+  last_key = None
+  for obj in objects:
+    if callback(obj):
+      updated += 1
+    total += 1
+
+    last_key = str(obj.key())
+  
+  return (total, updated, last_key)
+  
+def get_all_from_key(model_klass, start_key, max_records):  
   params = {}
   wheres = []
   
@@ -15,28 +54,12 @@ def update_created_at(start_key = None):
   else:
     wheres = ''
   
-  from tasks_data.models import Task
-  
-  tasks = Task.gql('%s ORDER BY __key__ ASC' % wheres, **params).fetch(max_records)
-  
-  total = 0
-  updated = 0
-  last_key = None
-  for task in tasks:
-    task.real_created_at = task.created_at
-    task.put()
-
-    updated += 1
-    total += 1
-
-    last_key = str(task.key())
-  
-  return (total, updated, last_key)
+  return model_klass.gql('%s ORDER BY __key__ ASC' % wheres, **params).fetch(max_records)
   
 MIGRATIONS = [
   {
-    'name': 'Update real crated_at times', 
-    'slug': 'update_created_at', 
-    'migration': update_created_at
+    'name': 'Create MRU contexts/projects', 
+    'slug': 'update_mru_lists', 
+    'migration': update_mru_lists
   }
 ]
