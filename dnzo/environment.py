6af76b63d80e6237f9831bf7e34df039fde462e3
tdawg <tdawg@c02d1e6f-6a35-45f2-ab14-3b6f79a691ff>
2009-04-08 02:07:46 +0000

    Modify manage.py and environment module to work outside of a webserver also.
    git-svn-id: svn+ssh://taylor-hughes.com/svnroot/dnzo/trunk@287 c02d1e6f-6a35-45f2-ab14-3b6f79a691ff

--- dnzo/environment.py
+++ dnzo/environment.py
@@ -1,7 +1,8 @@
 import os
 
-IS_DEVELOPMENT   = ('Dev' in os.getenv('SERVER_SOFTWARE'))
+software = os.getenv('SERVER_SOFTWARE')
+IS_DEVELOPMENT   = software and ('Dev' in software)
 IS_PRODUCTION    = not IS_DEVELOPMENT
 
 CURRENT_VERSION  = os.getenv('CURRENT_VERSION_ID')
-MAJOR_VERSION    = CURRENT_VERSION.split('.')[0]
+MAJOR_VERSION    = (CURRENT_VERSION or "").split('.')[0]
--- dnzo/manage.py
+++ dnzo/manage.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-from django.core.management import execute_manager
-try:
-    import settings # Assumed to be in the same directory.
-except ImportError:
-    import sys
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
-    sys.exit(1)
+if __name__ == '__main__':
+    from common.appenginepatch.aecmd import setup_env
+    setup_env(manage_py_env=True)
 
-if __name__ == "__main__":
+    import settings
+    from django.core.management import execute_manager
     execute_manager(settings)
