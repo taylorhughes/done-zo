e7c655bfdc860cd007e9c274c729f8a00d7fa0f5
tdawg <tdawg@c02d1e6f-6a35-45f2-ab14-3b6f79a691ff>
2009-05-31 20:14:59 +0000

    Add some more basic tests for /t/, which should return all tasks for a user.
    git-svn-id: svn+ssh://taylor-hughes.com/svnroot/dnzo/trunk@307 c02d1e6f-6a35-45f2-ab14-3b6f79a691ff

--- dnzo/test/test_api.py
+++ dnzo/test/test_api.py
@@ -10,13 +10,18 @@ from test.fixtures import setup_fixtures
 
 from tasks_data.models import Task
 
+BOGUS_IDS = ('abc', '-1', '0.1234', '1.', '.1', ' 123 ', '99999')
+
 class TaskAPITest(unittest.TestCase):
   def setUp(self):
     setup_fixtures()
     self.application = webapp.WSGIApplication(API_URLS, debug=True)
 
-  def test_task(self):
+  def test_get_task(self):
     app = TestApp(self.application)
+
+    all_tasks_response = app.get(path.join(API_PREFIX,'t'))
+    self.assertEqual('200 OK', all_tasks_response.status)
 
     for task in Task.all():
       task_id = str(task.key().id())
@@ -24,3 +29,15 @@ class TaskAPITest(unittest.TestCase):
       self.assertEqual('200 OK', response.status)
       self.assertTrue(json.dumps(task.body) in response,
                       "Response should include JSON-encoded task body.")
+      self.assertTrue(json.dumps(task.project) in response,
+                      "Response should include task's project.")
+
+      self.assertTrue(json.dumps(task.body) in all_tasks_response,
+                      "/t/ response should include all tasks' bodies.")
+      self.assertTrue(json.dumps(task.project) in all_tasks_response,
+                      "/t/ response should include all tasks' projects.")
+
+    for bogus_id in BOGUS_IDS:
+      response = app.get(path.join(API_PREFIX,'t',bogus_id), expect_errors=True)
+      self.assertTrue('404 Not Found' in response.status,
+                      "Bogus ID task should be Not Found, but response was (%s)" % response.status)
