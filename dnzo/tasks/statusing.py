1e9565480024d0b76554cbfd8647ee2ce09ea046
tdawg <tdawg@c02d1e6f-6a35-45f2-ab14-3b6f79a691ff>
2013-06-10 06:16:02 +0000

    Remove |escape since newer Django auto-escapes.
    git-svn-id: svn+ssh://taylor-hughes.com/svnroot/dnzo/trunk@430 c02d1e6f-6a35-45f2-ab14-3b6f79a691ff

--- dnzo/tasks/statusing.py
+++ dnzo/tasks/statusing.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*- 
+
 #
 #  Some methods for passing status messages between views. 
 #
@@ -11,7 +13,7 @@ class Statuses:
 MESSAGES = {
   # NOTE: These are output in straight HTML with no encoding. 
   Statuses.TASKS_PURGED:     "Completed tasks have been archived.",
-  Statuses.TASKS_NOT_PURGED: "No tasks to archive &mdash; complete some first!",
+  Statuses.TASKS_NOT_PURGED: "No tasks to archive — complete some first!",
   Statuses.TASK_DELETED:     "Task has been deleted.",
   Statuses.LIST_DELETED:     "List has been deleted.",
 }
